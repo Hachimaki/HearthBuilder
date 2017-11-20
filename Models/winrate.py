9b01174939f6a85655acdd3e8b98da6b48240be8
Tim Barao <tjb13b@my.fsu.edu>
2017-11-20 15:34:31 -0500

    Created method to process data into probability table for use of creating models

--- Models/winrate.py
+++ Models/winrate.py
@@ -4,21 +4,24 @@ import collections
 ''' "numGames" "winRate" "cID1" "cID2" "cID3" '''
 
 model = []
-numDecks = 10000
+numDecks = 1000
+totalCards = 30
+numArchs = 10
 
-for i in range(0, numDecks+1):
-    x = randint(1000, 10000)
-    y = round(uniform(0.40, 0.65), 2)
+for i in range(0, numDecks):
+    x = randint(1, numArchs)
+    y = randint(1000, 10000)
+    z = round(uniform(0.40, 0.65), 2)
 
-    m = {"numGames":x, "winRate":y}
+    m = {"type":x,"numGames":y, "winRate":z}
     deck = collections.OrderedDict(m)
     deck.update({"cd1":2})
     numCards = 2;
 
-    for i in range(2, 31):
+    for i in range(2, totalCards+1):
         card = "cd" + str(i)
         num = randint(0, 2)
-        if (numCards + num) < 30:
+        if (numCards + num) < 30:      #BINGO BANGO BONGO
             deck.update({card:num})
             numCards += num
         else:
@@ -26,50 +29,54 @@ for i in range(0, numDecks+1):
 
     model.append(deck);
 
-
 archmodel = collections.OrderedDict()
+cards = collections.OrderedDict()
 
-for j in range(1, 31):
-    numGames = 0
-    p = 0.0
-    q = 0
+for i in range(1, numArchs+1):
+    archmodel.update({str(i):collections.OrderedDict()})
 
-    for i in model:
-        #print "=-=-=-=-=-=-=-=-=Deck=-=-=-=-=-=-=-=-=-=-=-="
-        #print "NumGames: ", i["numGames"]
-        #print "Win Rate: ", i["winRate"]
-        #print "cd" + str(j) + " : ", i["cd" + str(j)]
-        '''p = p + (i['numGames'] * i['winRate'] * i['cd' + str(j)])'''
-        # if i['cd' + str(j)] != 0:
-        #g = (i['cd' + str(j)] * i['winRate']) #(i['cd' + str(j)] * i['winRate'])
+for i in range(1, totalCards+1):
+    cards.update({"cd" + str(i):0})
 
-        if i['cd' + str(j)] != 0:
-            p += (1 * i['winRate']) #(i['cd' + str(j)] * i['winRate'])
-            q += 1
+for i in archmodel:
+    for j in range(1, totalCards+1):
+        archmodel[i].update({'cd' + str(j):0})
+
+print model
+
+for i in model:
+    for j in range(1, totalCards+1):
+        if i['cd' + str(j)] > 0:
+            cards['cd' + str(j)] += 1
+
+for i in archmodel:
+    for j in model:
+        if j['type'] == int(i):
+            for k in range(1, totalCards+1):
+                if j['cd' + str(k)] > 0:
+                    archmodel[i].update({'cd' + str(k):archmodel[i]['cd'+str(k)] + 1})
+
+for i in archmodel:
+    for k, v in archmodel[i].items():
+        div = float(cards[k])
+        if(div != 0):
+            archmodel[i].update({k: round(float(v)/div, 2)})
+        else:
+            archmodel[i].update({k: 0})
+
+print "type ",
+
+for i in range(1, totalCards+1):
+    print "cd" + str(i),
+
+print ""
+
+for i in archmodel:
+    print i,
+    for k, v in archmodel[i].items():
+        print v,
+    print ""
 
 
-    if (q != 0):
-        archmodel.update({"cd"+str(j): round(p/q * 100, 2)})
-    else:
-        print "fuck my ass"
-        archmodel.update({"cd"+str(j):0})
-
-sum = 0
-for k,v in archmodel.items():
-    print k, str(v) + "%"
-    sum += v
-
-print sum/30
-
-
-
-
-'''
-winrate of deck =  wins / losses;
-winrate of card = ((wins / losses) * presence_of_card) / num_decks_card_appears
-'''
-
-
-
-
-
+for k,v in cards.items():
+    print k, v
