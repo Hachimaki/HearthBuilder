1b8e4e85a001d0a2f6e94ed5dab54f415a3abc1b
Andrew Sosa <andrewsosa001@gmail.com>
2017-12-03 21:13:47 -0500

    Add map of all the things

---
+++ HearthDB/data_to_file.py
@@ -0,0 +1,17 @@
+from vectors import get_decks, get_archs
+from vectors import card_map
+
+def main():
+
+    inv_map = {v: k for k, v in card_map.items()}
+    indexes = sorted(inv_map.keys())
+    ids = [inv_map[index] for index in indexes]
+    print('archetype, ' + ', '.join(ids))
+
+    for arch in get_archs():
+        for deck in get_decks(int(arch)):
+            vs = str(deck.to_vector()).replace('[', '').replace(']', '')
+            print(str(deck.arch_id) + ', ' + vs)
+
+if __name__ == '__main__':
+    main()
