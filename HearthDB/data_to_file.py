f2d0241662d2da5813dc6944d99392f225072a43
Andrew Sosa <andrewsosa001@gmail.com>
2017-12-10 17:09:41 -0500

    add crap

--- HearthDB/data_to_file.py
+++ HearthDB/data_to_file.py
@@ -6,12 +6,12 @@ def main():
     inv_map = {v: k for k, v in card_map.items()}
     indexes = sorted(inv_map.keys())
     ids = [inv_map[index] for index in indexes]
-    print('archetype, ' + ', '.join(ids))
+    print('win_rate, archetype, ' + ', '.join(ids))
 
     for arch in get_archs():
         for deck in get_decks(int(arch)):
             vs = str(deck.to_vector()).replace('[', '').replace(']', '')
-            print(str(deck.arch_id) + ', ' + vs)
+            print(str(deck.win_rate) + ', ' + str(deck.arch_id) + ', ' + vs)
 
 if __name__ == '__main__':
     main()
--- HearthDB/vectors.py
+++ HearthDB/vectors.py
@@ -1,12 +1,13 @@
 #pylint: disable=W0123,C0326
 import json
 
-card_map = json.load(open('vectors/card_map.json'))
+card_map = json.load(open('_vectors/card_map.json'))
 
 class Deck:
-    def __init__(self, arch_id: int, cards: list):
+    def __init__(self, arch_id: int, cards: list, win_rate: int):
         self.arch_id = arch_id
         self.cards = cards
+        self.win_rate = win_rate
 
     def to_vector(self) -> list:
         vector = [0 for i in range(len(card_map))]
@@ -21,7 +22,7 @@ def get_archs() -> list:
     """
     Returns a list of available archetypes
     """
-    with open('vectors/decks_by_archetype.json') as deckfile:
+    with open('_vectors/decks_by_archetype.json') as deckfile:
         decks_by_archetype = json.load(deckfile)
         return list(decks_by_archetype.keys())
 
@@ -30,10 +31,10 @@ def get_decks(arch_id: int=None) -> list:
     Returns a list of Deck objects for each deck in the archetype.
     Note that Deck.cards has had the popular cards removed.
     """
-    with open('vectors/decks_by_archetype.json') as deckfile:
+    with open('_vectors/decks_by_archetype.json') as deckfile:
         decks_by_archetype = json.load(deckfile)
         decks = list(decks_by_archetype.get(str(arch_id), list()))
-        return [Deck(arch_id, d['cards']) for d in decks]
+        return [Deck(arch_id, d['cards'], d['win_rate']) for d in decks]
 
 # Look up card names
 card_db = json.load(open('db.json'))
@@ -49,7 +50,7 @@ def get_card(dbf_id: int) -> dict:
 
 if __name__ == '__main__':
     # Read cards dataset
-    with open('dataset.json') as dataset, open('vectors/most_popular_cards.json') as most_common:
+    with open('dataset.json') as dataset, open('_vectors/most_popular_cards.json') as most_common:
         common = set(json.load(most_common)['most_common'])
 
         all_decks = json.load(dataset)['series']['data']
@@ -60,14 +61,16 @@ if __name__ == '__main__':
                 cards = eval(deck['deck_list'])
                 cards = list(filter((lambda pair: pair[0] not in common), cards))
                 arch = deck['archetype_id']
+                win_rate = deck['win_rate']
 
                 if arch not in all_archetypes.keys():
                     all_archetypes[arch] = list()
 
                 all_archetypes[arch].append({
                     'cards': cards,
-                    'archetype_id': arch
+                    'archetype_id': arch,
+                    'win_rate': win_rate
                 })
 
-        with open('vectors/decks_by_archetype.json', 'w') as outfile:
+        with open('_vectors/decks_by_archetype.json', 'w') as outfile:
             outfile.write(json.dumps(all_archetypes))
